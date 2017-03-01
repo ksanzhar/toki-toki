483d9ddb3db4627a02307699cecf349af593b9f4
ksanzhar <ksanzhar@gmail.com>
2017-03-01 21:31:24 +0600

    Initial Commit

---
+++ toki-toki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  toki-toki
+//
+//  Created by Sanzhar Kozhahmetov on 3/1/17.
+//  Copyright © 2017 Sanzhar Kozhahmetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
