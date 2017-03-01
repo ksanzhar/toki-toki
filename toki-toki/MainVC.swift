e8234b02393cdccf7caf25e478140b515ed0b7ca
ksanzhar <ksanzhar@gmail.com>
2017-03-01 22:40:39 +0600

    Firebase init

--- toki-toki/MainVC.swift
+++ toki-toki/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  toki-toki
 //
 //  Created by Sanzhar Kozhahmetov on 3/1/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
