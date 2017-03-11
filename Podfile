71693ed498a74e5ad2eccc56f3a9850404c21af3
ksanzhar <ksanzhar@gmail.com>
2017-03-11 07:59:25 +0600

    Add facebook login

--- Podfile
+++ Podfile
@@ -10,5 +10,6 @@ target 'toki-toki' do
   pod 'Firebase/Auth'
   pod 'Firebase/Database'
   pod 'Firebase/Storage'
+  pod 'FBSDKLoginKit'
 
 end
--- toki-toki/MainVC.swift
+++ toki-toki/MainVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class MainVC: UIViewController {
 
@@ -20,6 +23,32 @@ class MainVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func fbBtnPressed(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print ("Unable to login in facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print ("User canceled facebook login")
+            } else {
+                print ("Facebook login")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+        
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print ("Unable to login in firebase")
+            } else {
+                print("Firebase login")
+            }
+        })
+    }
 
 }
 
